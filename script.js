e0ddaaea0fa0ce55980aa9795decd9b91d33d5a7
Abhijeet soren <154517350+Abhijxxt14@users.noreply.github.com>
2024-09-30 23:41:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 
 let changingWords = ['Style', 'Elegance', 'Minimalism', 'Comfort'];
 let wordIndex = 0;
-
 function changeWord() {
     document.getElementById('changing-word').textContent = changingWords[wordIndex];
     wordIndex = (wordIndex + 1) % changingWords.length;
 }
-
 setInterval(changeWord, 3000);
-
 let carouselIndex = 0;
 const carouselItems = document.querySelectorAll('.carousel-item');
-function showCarouselItem() {
-    carouselItems.forEach(item => item.style.display = 'none');
-    carouselItems[carouselIndex].style.display = 'block';
-    carouselIndex = (carouselIndex + 1) % carouselItems.length;
-}
+const descriptions = [
+    'Safe and Long-lasting.',
+    'Stability on Any Terrain.',
+    'Ride with Confidence.',
+    'Raw and Practical.'
+   ];
+const descriptionBox = document.querySelector('.description-box .description');
 
-setInterval(showCarouselItem, 5000);
+function showNextItem() {
+ carouselItems.forEach(item => item.style.transform = `translateX(-${carouselIndex * 100}%)`); 
+descriptionBox.textContent = descriptions[carouselIndex];
+carouselIndex = (carouselIndex + 1) % carouselItems.length;
+}
+setInterval(showNextItem, 5000);
+showNextItem();
